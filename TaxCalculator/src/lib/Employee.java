9d62e9afcad3f51b4814e86b2baa6e2e58b56650
afanmr07 <afanmaulidrizqi@student.telkomuniversity.ac.id>
2022-04-20 06:46:45 +0700

    edit2

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -10,6 +10,7 @@ public class Employee {
 	private String employeeId;
 	private String firstName;
 	private String lastName;
+	private String idNumber;
 	private String address;
 
 	private int yearJoined;
